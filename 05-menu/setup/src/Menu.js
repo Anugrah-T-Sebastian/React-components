2ce8671f8aa7e7db99a631e3a159f8779dd22990
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2022-03-18 23:20:33 +0530

    Menu component added

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ menuItems }) => {
+
+  return (
+    <div className='section-center'>
+      {menuItems.map((item) => {
+        const { id, title, category, price, img, desc } = item;
+        return (
+          
+        )
+      })}
+    </div>
+  );
 };
 
 export default Menu;
