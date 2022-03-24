31d79cc986417f8d73f7a78ee046447ff8bafc8a
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2022-03-24 23:35:20 +0530

    Grocery bud added

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,25 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup added</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  }
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}></form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn' >clear items</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
