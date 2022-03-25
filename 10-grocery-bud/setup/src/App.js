7d0d32553467d8303248fc187fb8da52678d27c8
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2022-03-25 23:56:38 +0530

    Grocery bud code updated

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -11,15 +11,37 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('form submitted');
+    if (!name) {
+      // display alert
+    }
+    else if (name && isEditing) {
+      //DEAL with editing
+    }
+    else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('');
+    }
   }
   return (
     <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}></form>
-      <div className='grocery-container'>
-        <List />
-        <button className='clear-btn' >clear items</button>
-      </div>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input type="text" className='grocery' placeholder='e.g. eggs' value={name} onChange={(e) => setName(e.target.value)} />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn' >clear items</button>
+        </div>
+      )}
     </section>
   )
 }
