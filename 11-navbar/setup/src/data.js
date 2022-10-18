ed8b0f28aa4d27da701d667cf7405a8ad64454b6
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2022-10-18 22:08:44 +0530

    Navbar component added

--- 11-navbar/setup/src/data.js
+++ 11-navbar/setup/src/data.js
@@ -4,7 +4,7 @@ export const links = [
   {
     id: 1,
     url: '/',
-    text: 'random',
+    text: 'home',
   },
   {
     id: 2,
@@ -25,13 +25,13 @@ export const links = [
     id: 5,
     url: '/profile',
     text: 'profile',
-  },
+  }
 ]
 
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: 'https://www.facebook.com',
     icon: <FaFacebook />,
   },
   {
@@ -41,7 +41,7 @@ export const social = [
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com',
     icon: <FaLinkedin />,
   },
   {
